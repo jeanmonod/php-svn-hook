4d22e49e7503bf18206038d93c6664fde44e746d
Jeanmonod David <david@idael.ch>
2011-09-06 09:52:10 +0300

    Update copy/paste error in the EmptyCommentCheck test

--- test/checks/EmptyCommentCheckTest.php
+++ test/checks/EmptyCommentCheckTest.php
@@ -7,16 +7,12 @@ $t = new lime_test(5, new lime_output_color());
 // Load dependency
 include_once dirname(__FILE__).'/../../checks/EmptyCommentCheck.class.php';
 
-$c = new EmptyCommentCheck('Simple file with no tab');
+$c = new EmptyCommentCheck('Normal commit message');
 $c->runCheck(array('file1'=>array('line1', 'line2')));
-$t->ok(!$c->fail(),"The check is not failling if there is no tab in the files");
+$t->ok(!$c->fail(),"The check is not failling if there is a good commit msg");
   
 $c = new EmptyCommentCheck('toto');
 $c->runCheck(array());
-$t->ok($c->fail(),"The check works when comment is to small");
+$t->ok($c->fail(),"The check fail when comment msg is too small");
 $t->is($c->renderErrorSummary(), "Commit message empty or too short", "A valid summary message is return");
 $t->is($c->renderErrorDetail(), "Minimum size is 5 characters", "A valid detail message is return");
-
-$c = new EmptyCommentCheck('Long comment');
-$c->runCheck(array());
-$t->ok(!$c->fail(),"The check is working when comment is long enought");
